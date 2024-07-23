b0784e32a73293a2bb0c051a018ef48e1255d57a
ValeriaRodriguezJ <rodriguz95@gmail.com>
2024-07-22 20:49:34 -0500

    feat: add functionality favorites

--- src/components/Book/index.tsx
+++ src/components/Book/index.tsx
@@ -1,31 +1,57 @@
-import React from 'react'
 import './book.css'
 import { BookItemType } from 'src/services/interfaces'
 
-export type BookType = {
-  id: string
-  volumeInfo: {
-    title: string
-    imageLinks: {
-      thumbnail: string
-    }
-  }
-}
+import { CiHeart } from 'react-icons/ci'
+import { FaHeart } from 'react-icons/fa'
+
+import { FaComment } from 'react-icons/fa'
+import { bookSavedType } from '@components/Books/interfaces'
+import { useEffect, useState } from 'react'
+
 export interface BookProps {
   book: BookItemType
+  isActionable: boolean
+  onActionBook: (id: string, action: string, comment?: string) => void
+  extraInfo?: bookSavedType
 }
 
-const Book = ({ book }: BookProps) => (
-  <div className="book">
-    <div className="book-image">
-      {book.volumeInfo.imageLinks ? (
-        <img alt={book.volumeInfo.title} src={book.volumeInfo.imageLinks.thumbnail || ''} />
-      ) : (
-        <img src="https://picsum.photos/200/260" alt="default" />
+const Book = ({ book, isActionable, onActionBook, extraInfo }: BookProps) => {
+  const [actionsBook, setActionsBook] = useState<bookSavedType | null>(null)
+
+  useEffect(() => {
+    if (extraInfo) {
+      setActionsBook(extraInfo)
+    }
+  }, [extraInfo])
+
+  return (
+    <div className="container">
+      <div className="book">
+        <div className="book-image">
+          {book.volumeInfo.imageLinks ? (
+            <img
+              alt={book.volumeInfo.title}
+              src={book.volumeInfo.imageLinks.thumbnail || ''}
+            />
+          ) : (
+            <img src="https://picsum.photos/200/260" alt="default" />
+          )}
+        </div>
+        <p className="book-title">{book.volumeInfo.title}</p>
+      </div>
+      {isActionable && (
+        <div className="container-action">
+          <button className="heart-button" onClick={() => onActionBook(book.id, 'like')}>
+            {actionsBook?.isLike && <FaHeart className="heart-icon red-heart" />}
+            {!actionsBook?.isLike && <CiHeart className="heart-icon" />}
+          </button>
+          <button className="comment-button">
+            <FaComment className="comment-icon" />
+          </button>
+        </div>
       )}
     </div>
-    <p className="book-title">{book.volumeInfo.title}</p>
-  </div>
-)
+  )
+}
 
 export default Book
--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -1,16 +1,78 @@
-import React from 'react'
+import { act, useState, useEffect } from 'react'
 import Book from '../Book'
 import './books.css'
 import { BookItemType } from 'src/services/interfaces'
+import { bookSavedType } from './interfaces'
 
 export interface BooksProps {
   books: BookItemType[]
+  isActionable?: boolean
 }
 
-const Books = ({ books }: BooksProps) => {
+const Books = ({ books, isActionable = false }: BooksProps) => {
+  const [booksSaved, setBooksSaved] = useState<bookSavedType[]>([])
+
+  const loadBookFromLocalStorage = () => {
+    const currentBooks = JSON.parse(localStorage.getItem('books') || '[]') || []
+    setBooksSaved(currentBooks)
+  }
+
+  const handleSaveBook = (id: string, action: string, comment?: string) => {
+    const booksSaved = JSON.parse(localStorage.getItem('books') || '[]') || []
+
+    let valueToSave = [...booksSaved]
+    const findBook = booksSaved.findIndex((item: bookSavedType) => item.id === id)
+    let copyCurrentBook = booksSaved[findBook]
+    if (findBook) {
+      copyCurrentBook
+    }
+
+    switch (action) {
+      case 'like':
+        if (findBook !== -1) {
+          valueToSave.splice(findBook, 1, {
+            ...copyCurrentBook,
+            isLike: !copyCurrentBook.isLike
+          })
+          break
+        } else {
+          valueToSave = [...valueToSave, { id, isLike: true, comments: [] }]
+          break
+        }
+      case 'comment':
+        if (findBook !== -1) {
+          valueToSave.splice(findBook, 1, {
+            ...copyCurrentBook,
+            comments: [...copyCurrentBook.comments, comment || '']
+          })
+          break
+        } else {
+          valueToSave = [...valueToSave, { id, isLike: false, comments: [comment || ''] }]
+          break
+        }
+    }
+
+    localStorage.setItem('books', JSON.stringify(valueToSave))
+    setBooksSaved(valueToSave)
+  }
+
+  useEffect(() => {
+    if (isActionable) {
+      void loadBookFromLocalStorage()
+    }
+  }, [])
+
   return (
     <div className="books">
-      {books.map(book => <Book key={book.id} book={book} />)} 
+      {books.map(book => (
+        <Book
+          key={book.id}
+          book={book}
+          isActionable={isActionable}
+          onActionBook={handleSaveBook}
+          extraInfo={booksSaved.find((item: bookSavedType) => item.id === book.id)}
+        />
+      ))}
     </div>
   )
 }
